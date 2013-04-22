7745776714a24436a0f6c0b0d6675fef6d7e430e
Chase Merlin <chasemerlin@gmail.com>
2013-04-22 15:10:21 -0400

    get game working

--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'sinatra'
+require 'pry'
 require './card'
 require './deck'
 require './player'
@@ -11,12 +12,16 @@ require './game'
 set :sessions, true
 
 get '/' do
-	session[:user] = nil
-	if session[:user] == nil
+	if session[:user].nil?
 		redirect '/form'
 	else
 		redirect 'game'
 	end
+end
+
+get '/reset' do
+	session[:user] = nil
+	redirect '/'
 end
 
 get '/form' do
@@ -24,13 +29,47 @@ get '/form' do
 end
 
 post '/username' do
-	session[:user] = params['username']
+	if params[:username].nil? || params[:username].empty?
+		@error = "Name can't be blank"
+		halt erb :form # Use halt to early return
+	end
+	session[:user] = params[:username]
 	redirect '/game'
 end
 
 get '/game' do
-	@game = Game.new
-	@game.deal_cards
+	session[:dealer_turn] = false
+	session[:game] = Game.new
+	session[:game].deal_cards
+	erb :game
+end
+
+get '/hit' do
+	session[:game].player.hand.cards << session[:game].deck.deal_a_card
+	if session[:game].player.is_busted?
+		@error = "You busted! You lose"
+	end
+	erb :game
+end
+
+get '/stay' do
+	session[:dealer_turn] = true
+	while session[:game].dealer.hand.total < 17
+		session[:game].dealer.hand.cards << session[:game].deck.deal_a_card
+		if session[:game].dealer.is_busted?
+			@success = "Dealer busted! You win!"
+			halt erb :game
+		end
+	end
+	player_total = session[:game].player.hand.total
+	dealer_total = session[:game].dealer.hand.total
+	if player_total > dealer_total
+		@success = "You Win!"
+	elsif dealer_total > player_total
+		@error = "You Lose!"
+	else 
+		@alert = "You Tie!"
+	end
 	erb :game
 end
 
